5262af9b7724e885f13f11d1ea61e5032c69e083
chancellor-whitaker <chancellor.whitaker@eku.edu>
2026-06-08 16:30:17 -0400

    tired

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,205 @@
 // import Component from "./widgets/example";
 
 import useData from "./hooks/useData";
+import Widget from "./widgets/example/Widget";
+
+const keyToLabel = { kpi: "Student Performance Indicator" };
+
+const getColumns = (rows) => {
+  return Object.keys(rows.length > 0 ? rows[0] : {}).map((key) => ({
+    key,
+    label: key in keyToLabel ? keyToLabel[key] : key,
+  }));
+};
 
 export default function App() {
-  const data = useData(
+  const response = useData(
     "https://irserver2.eku.edu/Apps/DataPage/PROD/Accreditation/ATMAE/data",
   );
 
-  const obj = data ? data : {};
+  const data = response ? response : {};
 
-  const metrics = Object.entries(obj)
+  const tuitionData =
+    "Tuition" in data && Array.isArray(data.Tuition) && data.Tuition.length > 0
+      ? data.Tuition[0]
+      : {};
+
+  const scholarData =
+    "Scholarship" in data &&
+    Array.isArray(data.Scholarship) &&
+    data.Scholarship.length > 0
+      ? data.Scholarship[0]
+      : {};
+
+  const metrics = Object.entries(data)
     .filter(([, v]) => Array.isArray(v) && v.length > 1)
     .map(([k]) => k)
     .sort();
 
-  const programs = [
-    ...new Set(
-      Object.entries(obj)
-        .filter(([, v]) => Array.isArray(v) && v.length > 1)
-        .map(([, v]) => v.map(({ p_title }) => p_title))
-        .flat()
-        .filter(Boolean),
-    ),
-  ].sort();
+  // const programs = [
+  //   ...new Set(
+  //     Object.entries(data)
+  //       .filter(([, v]) => Array.isArray(v) && v.length > 1)
+  //       .map(([, v]) => v.map(({ p_title }) => p_title))
+  //       .flat()
+  //       .filter(Boolean),
+  //   ),
+  // ].sort();
+
+  const getRowData = () => {
+    const lookup = {};
+
+    Object.entries(data).forEach(([a, b]) => {
+      if (metrics.includes(a)) {
+        b.forEach(({ p_title, ...rest }) => {
+          const standardized = Object.fromEntries(
+            Object.entries(rest).map(([k, v]) => [`${a}→${k}`, v]),
+          );
+
+          if (!(p_title in lookup)) lookup[p_title] = { p_title };
+
+          lookup[p_title] = { ...lookup[p_title], ...standardized };
+        });
+      }
+    });
+
+    return Object.values(lookup);
+  };
+
+  const rowData = getRowData();
+
+  const flipRowData = () => {
+    const kpis = Object.keys(rowData.length > 0 ? rowData[0] : {}).filter(
+      (key) => key !== "p_title",
+    );
+
+    const lookup = Object.fromEntries(kpis.map((kpi) => [kpi, { kpi }]));
+
+    kpis.forEach((kpi) =>
+      rowData.forEach((row) => {
+        lookup[kpi][row.p_title] = row[kpi];
+      }),
+    );
+
+    return Object.values(lookup);
+  };
+
+  const flippedRowData = flipRowData();
+
+  const tableRows = flippedRowData;
+
+  const isFlipped = tableRows === flippedRowData;
+
+  const columns = getColumns(tableRows);
+
+  const valueFormatter = ({ value, row, column }) => {
+    const metric = isFlipped ? row.kpi : column.key;
+
+    const str = metric.split("→")[metric.split("→").length - 1];
+
+    if (
+      typeof value === "number" &&
+      ["final_gpa", "final_hours", "years_to_grad"].includes(str.toLowerCase())
+    ) {
+      return +value.toFixed(2);
+    }
+    return value;
+  };
+
+  const tableAccessor = ({ data, columns }) => {
+    if (!isFlipped) {
+      return {
+        data,
+        columns: columns.filter((key) => {
+          const str = key.split("→")[key.split("→").length - 1];
+          return !["time_frame", "updated_date"].includes(str.toLowerCase());
+        }),
+      };
+    }
+
+    return {
+      columns,
+      data: data.filter(({ kpi }) => {
+        const str = kpi.split("→")[kpi.split("→").length - 1];
+        return !["time_frame", "updated_date"].includes(str.toLowerCase());
+      }),
+    };
+  };
 
   console.log("data", data);
 
-  console.log("metrics", metrics);
+  // console.log("metrics", metrics);
 
-  console.log("programs", programs);
+  // console.log("programs", programs);
+
+  // const valueFormatter=({value,row,column})=>
+
+  /*
+    {
+        as_of_date: '29MAY2026',
+        gr_per_cred_charge: 607,
+        ug_max_charge: 5220,
+        ug_per_cred_charge: 435,
+        updated_date: '05/29/2026',
+        website: 'https://www.eku.edu/tuition-and-aid/',
+        year: '2526'
+      }
+    */
+
+  // ug max charge
+
+  const formatterUSD = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
 
   return (
     <>
-      <div>
-        <h1>React app</h1>
-        <hr></hr>
-      </div>
+      <Widget
+        valueFormatter={valueFormatter}
+        data={tableRows}
+        columns={columns}
+        tableAccessor={tableAccessor}
+        below={
+          <ul>
+            <li>
+              <strong>N/A =</strong> Fewer than five graduates during the
+              reporting period
+            </li>
+            <li>
+              <strong>
+                Tuition expenses for program completion (AY 20
+                {(typeof tuitionData.year === "string"
+                  ? tuitionData.year
+                  : ""
+                ).substring(0, 2)}
+                –20
+                {(typeof tuitionData.year === "string"
+                  ? tuitionData.year
+                  : ""
+                ).substring(2)}
+                ):
+              </strong>
+              &nbsp;120 credit hours at the current tuition rate of{" "}
+              {formatterUSD.format(tuitionData.ug_per_cred_charge)} per hour
+              equals {formatterUSD.format(tuitionData.ug_max_charge * 8)}.{" "}
+              <a href={tuitionData.website}>
+                See Undergraduate Cost of Attendance page
+              </a>
+              &nbsp;for details.
+            </li>
+            <li>
+              <strong>Availability of awards and scholarships:</strong>
+              &nbsp;Information about all scholarships and applications is
+              available on the{" "}
+              <a href={scholarData.website}>
+                Eastern Kentucky University Scholarships page
+              </a>
+              .
+            </li>
+          </ul>
+        }
+      ></Widget>
     </>
   );
 }
--- src/widgets/example/Widget.jsx
+++ src/widgets/example/Widget.jsx
@@ -1,8 +1,43 @@
-export default function Widget() {
+import { Fragment } from "react";
+
+export default function Widget({
+  columns: cols,
+  data: rows,
+  tableAccessor = (obj) => obj,
+  valueFormatter = ({ value }) => value,
+  above = <h3>Student performance and achievement information</h3>,
+  below = (
+    <ul>
+      <li>
+        <strong>N/A =</strong> Fewer than five graduates during the reporting
+        period
+      </li>
+      <li>
+        <strong>Tuition expenses for program completion (AY 2025–2026):</strong>
+        &nbsp;120 credit hours at the current tuition rate of $201.90 per hour
+        equals $24,228.{" "}
+        <a href="https://www.wsc.edu/cost/cost-of-attendance">
+          See Undergraduate Cost of Attendance page
+        </a>
+        &nbsp;for details.
+      </li>
+      <li>
+        <strong>Availability of awards and scholarships:</strong>
+        &nbsp;Information about all scholarships and applications is available
+        on the{" "}
+        <a href="https://www.wsc.edu/scholarships">
+          Wayne State College Scholarships page
+        </a>
+        .
+      </li>
+    </ul>
+  ),
+}) {
+  const { data, columns } = tableAccessor({ data: rows, columns: cols });
   return (
     <div className="page-content">
       <div className="editor">
-        <h3>Student performance and achievement information</h3>
+        {above}
         <div
           className="table-wrapper"
           role="region"
@@ -10,120 +45,60 @@ export default function Widget() {
           tabIndex={0}
         >
           <table style={{ width: "100%" }}>
-            <caption id={86019}>
+            {/* <caption id={86019}>
               <p style={{ textAlign: "left" }}>
                 The following job placement data represents the academic period
                 of Dec. 2023 - Aug. 2024, unless otherwise stated.
               </p>
-            </caption>
+            </caption> */}
             <thead>
               <tr>
-                <th scope="col" style={{ textAlign: "center" }}>
-                  Student Performance Indicator
-                </th>
-                <th scope="col" style={{ textAlign: "center" }}>
-                  Industrial Technology – Construction Management
-                </th>
-                <th scope="col" style={{ textAlign: "center" }}>
-                  Industrial Technology - Drafting &amp; Design
-                </th>
-                <th scope="col" style={{ textAlign: "center" }}>
-                  Industrial Technology – Manufacturing Management
-                </th>
-                <th scope="col" style={{ textAlign: "center" }}>
-                  Industrial Technology – Safety Management
-                </th>
-                <th scope="col" style={{ textAlign: "center" }}>
-                  Engineering Technology
-                </th>
+                {columns.map((column) => (
+                  <th
+                    style={{ textAlign: "center" }}
+                    key={column.key}
+                    scope="col"
+                  >
+                    {column.label}
+                  </th>
+                ))}
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>
-                  <p>Graduation Rate (% Graduated in 4 years)</p>
-                </td>
-                <td style={{ textAlign: "center" }}>64%</td>
-                <td style={{ textAlign: "center" }}>63%</td>
-                <td style={{ textAlign: "center" }}>N/A</td>
-                <td style={{ textAlign: "center" }}>N/A</td>
-                <td style={{ textAlign: "center" }}>40%</td>
-              </tr>
-              <tr>
-                <td>
-                  <p>Retention Rate (Average for 5 years ending 2024)</p>
-                </td>
-                <td style={{ textAlign: "center" }}>81%</td>
-                <td style={{ textAlign: "center" }}>87%</td>
-                <td style={{ textAlign: "center" }}>
-                  <abbr title="Not available">N/A</abbr>
-                </td>
-                <td style={{ textAlign: "center" }}>90%</td>
-                <td style={{ textAlign: "center" }}>60%</td>
-              </tr>
-              <tr>
-                <td>
-                  <p>Mean GPA -Graduating Class (5 years ended May, 2025)</p>
-                </td>
-                <td style={{ textAlign: "center" }}>3.41</td>
-                <td style={{ textAlign: "center" }}>3.56</td>
-                <td style={{ textAlign: "center" }}>
-                  <abbr title="Not available">N/A</abbr>
-                </td>
-                <td style={{ textAlign: "center" }}>3.34</td>
-                <td style={{ textAlign: "center" }}>3.41</td>
-              </tr>
-              <tr>
-                <td>
-                  Average Years to Complete the Degree (most recent cohort)
-                </td>
-                <td style={{ textAlign: "center" }}>3.75</td>
-                <td style={{ textAlign: "center" }}>3.63</td>
-                <td style={{ textAlign: "center" }}>
-                  <abbr title="Not available">N/A</abbr>
-                </td>
-                <td style={{ textAlign: "center" }}>4.13</td>
-                <td style={{ textAlign: "center" }}>3.92</td>
-              </tr>
-              <tr>
-                <td>Career Placement Rate</td>
-                <td style={{ textAlign: "center" }}>100%</td>
-                <td style={{ textAlign: "center" }}>100%</td>
-                <td style={{ textAlign: "center" }}>
-                  <abbr title="Not available">N/A</abbr>
-                </td>
-                <td style={{ textAlign: "center" }}>100%</td>
-                <td style={{ textAlign: "center" }}>100%</td>
-              </tr>
+              {data.map((row, rowIndex) => (
+                <tr key={rowIndex}>
+                  {columns.map((column, colIndex) => (
+                    <Fragment key={column.key}>
+                      {colIndex === 0 ? (
+                        <td data-label={column.label}>
+                          <p>
+                            {valueFormatter({
+                              value: row[column.key],
+                              column,
+                              row,
+                            })}
+                          </p>
+                        </td>
+                      ) : (
+                        <td
+                          style={{ textAlign: "center" }}
+                          data-label={column.label}
+                        >
+                          {valueFormatter({
+                            value: row[column.key],
+                            column,
+                            row,
+                          })}
+                        </td>
+                      )}
+                    </Fragment>
+                  ))}
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
-        <ul>
-          <li>
-            <strong>N/A =</strong> Fewer than five graduates during the
-            reporting period
-          </li>
-          <li>
-            <strong>
-              Tuition expenses for program completion (AY 2025–2026):
-            </strong>
-            &nbsp;120 credit hours at the current tuition rate of $201.90 per
-            hour equals $24,228.{" "}
-            <a href="https://www.wsc.edu/cost/cost-of-attendance">
-              See Undergraduate Cost of Attendance page
-            </a>
-            &nbsp;for details.
-          </li>
-          <li>
-            <strong>Availability of awards and scholarships:</strong>
-            &nbsp;Information about all scholarships and applications is
-            available on the{" "}
-            <a href="https://www.wsc.edu/scholarships">
-              Wayne State College Scholarships page
-            </a>
-            .
-          </li>
-        </ul>
+        {below}
         <p>&nbsp;</p>
       </div>
     </div>
